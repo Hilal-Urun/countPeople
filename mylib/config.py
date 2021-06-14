46a80afcb110a795f56c48af3fc86145f28c2376
Hilal ÜRÜN <hilal.urun@std.antalya.edu.tr>
2021-06-14 11:55:36 +0300

    Initial commit

---
+++ mylib/config.py
@@ -0,0 +1,16 @@
+
+MAIL = ''
+url ='rtsp://admin:admin123@10.100.7.109:554/cam/realmonitor?channel=1&subtype=0'
+
+# ON/OFF for mail feature. Enter True to turn on the email alert feature.
+ALERT = False
+# Set max. people inside limit. Optimise number below: 10, 50, 100, etc.
+Threshold = 10
+# Threading ON/OFF
+Thread = True
+# Simple log to log the counting data
+Log = True
+# Auto run/Schedule the software to run at your desired time
+Scheduler = False
+# Auto stop the software after certain a time/hours
+Timer = False
